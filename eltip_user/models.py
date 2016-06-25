fea78ac5e1bbd5cc10ee050cf0f986452aaea17d
Ubuntu <ubuntu@ip-172-31-21-232.ap-northeast-1.compute.internal>
2016-06-25 08:10:19 +0000

    init

---
+++ eltip_user/models.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from uuid import uuid4
+from django.utils.deconstruct import deconstructible
+from django.conf import settings
+
+@deconstructible
+class PathAndRename(object):
+    def __init__(self, path):
+        self.sub_path = path
+    def __call__(self, instance, filename):
+        ext = filename.split('.')[-1]
+        # get filename
+        if instance.pk:
+            filename = '{}.{}'.format(instance.pk, ext)
+        else:
+            # set filename as random string
+            filename = '{}.{}'.format(uuid4().hex, ext)
+        # return the whole path to the file
+        return os.path.join(self.sub_path, filename)
+
+class User(models.Model):
+    createdate = models.DateTimeField(auto_now_add=True)
+    updatedate = models.DateTimeField(auto_now=True)
+    account = models.CharField(blank=False, max_length=20, unique=True)
+    password = models.CharField(blank=False, max_length=20)
+    identity = models.CharField(blank=False, max_length=1)
+    name = models.CharField(blank=True, null=True, max_length=20, default="")
+    sex = models.CharField(blank=True, null=True, max_length=2, default="")
+    birthday = models.DateField(null=True)
+    email = models.EmailField(unique=True)
+    position = models.CharField(blank=True, null=True, max_length=20, default="")
+    company = models.CharField(blank=True, null=True, max_length=60, default="")
+
+class Account(models.Model):
+    createdate = models.DateTimeField(auto_now_add=True)
+    updatedate = models.DateTimeField(auto_now=True)
+    uid = models.IntegerField()
+    credit = models.IntegerField()
+    balance = models.IntegerField()
