08d9c34181193b4ef3a8e3beb39056610d17e967
Song <sooon1g1@gmail.com>
2016-06-25 16:50:10 +0800

    add templates

--- eltip/urls.py
+++ eltip/urls.py
@@ -19,3 +19,8 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
